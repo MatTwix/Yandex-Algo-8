c1e198d42fa97fc73422c3bee10e17dca6f37577
Matvey Fedorov <mfgolden@yandex.ru>
2025-10-06 16:54:09 +0300

    hw1/j

--- hw1/j/j.go
+++ hw1/j/j.go
@@ -7,242 +7,188 @@ import (
 	"strings"
 )
 
-type Disk struct {
-	memory        []int
-	firstFreeCell int
+type Namespace struct {
+	storage map[string]GenericList
 }
 
-func NewDisk(size int) *Disk {
-	return &Disk{memory: make([]int, size)}
+func NewNamespace() *Namespace {
+	return &Namespace{
+		storage: make(map[string]GenericList),
+	}
 }
 
-func (d *Disk) Allocate(size int) int {
-	addr := d.firstFreeCell
-	d.firstFreeCell += size
-	return addr
+func (n *Namespace) Get(name string) GenericList {
+	return n.storage[name]
 }
 
-type Sequence interface {
-	Get(idx int) int
-	Set(idx, x int)
-	StartPtr() int
-	Disk() *Disk
+func (n *Namespace) Set(name string, value GenericList) {
+	n.storage[name] = value
+}
+
+type GenericList interface {
+	Get(i int) int
+	Set(i int, value int)
+	Sublist(from, to int) GenericList
+	Length() int
+	Add(value int)
 }
 
 type List struct {
-	name     string
-	startPtr int
-	len      int
-	capacity int
-	disk     *Disk
+	data       []int
+	isRootList bool
 }
 
-func nextTwoPower(n int) int {
-	if n <= 1 {
-		return 1
-	}
-	p := 1
-	for p < n {
-		p <<= 1
-	}
-	return p
-}
-
-func NewList(disk *Disk, name string, elems ...int) *List {
-	length := len(elems)
-	cap := nextTwoPower(length)
-
-	start := disk.Allocate(cap)
-	for idx, el := range elems {
-		disk.memory[start+idx] = el
-	}
-
+func NewList(data []int) *List {
 	return &List{
-		name:     name,
-		startPtr: start,
-		len:      length,
-		capacity: cap,
-		disk:     disk,
+		data:       data,
+		isRootList: true,
 	}
 }
 
-func (l *List) Add(x int) {
-	if l.len == l.capacity {
-		newCapacity := l.capacity * 2
-		newStart := l.disk.Allocate(newCapacity)
-		copy(l.disk.memory[newStart:], l.disk.memory[l.startPtr:l.startPtr+l.len])
-		l.startPtr = newStart
-		l.capacity = newCapacity
-	}
-
-	l.disk.memory[l.startPtr+l.len] = x
-	l.len++
+func (l *List) Get(i int) int {
+	return l.data[i]
 }
 
-func (l *List) Set(idx, x int) {
-	l.disk.memory[l.startPtr+idx] = x
+func (l *List) Set(i int, value int) {
+	l.data[i] = value
 }
 
-func (l *List) Get(idx int) int {
-	return l.disk.memory[l.startPtr+idx]
+func (l *List) Add(value int) {
+	l.data = append(l.data, value)
 }
 
-func (l *List) StartPtr() int {
-	return l.startPtr
+func (l *List) Sublist(from, to int) GenericList {
+	return NewSublist(l, from, to)
 }
 
-func (l *List) Disk() *Disk {
-	return l.disk
+func (l *List) Length() int {
+	return len(l.data)
 }
 
 type Sublist struct {
-	name     string
-	parent   Sequence
-	startIdx int
-	endIdx   int
-	len      int
+	parent     GenericList
+	start      int
+	end        int
+	isRootList bool
 }
 
-func NewSublist(name string, parent Sequence, startIdx, endIdx int) *Sublist {
-	startIdx--
-	endIdx--
-
+func NewSublist(parent GenericList, start, end int) *Sublist {
 	return &Sublist{
-		name:     name,
-		parent:   parent,
-		startIdx: startIdx,
-		endIdx:   endIdx,
-		len:      endIdx - startIdx + 1,
+		parent:     parent,
+		start:      start,
+		end:        end,
+		isRootList: false,
 	}
 }
 
-func (s *Sublist) Set(idx, x int) {
-	basePrt := s.parent.StartPtr()
-	disk := s.parent.Disk()
-	disk.memory[basePrt+s.startIdx+idx] = x
+func (s *Sublist) Get(i int) int {
+	return s.parent.Get(s.start + i)
 }
 
-func (s *Sublist) Get(idx int) int {
-	basePrt := s.parent.StartPtr()
-	disk := s.parent.Disk()
-	return disk.memory[basePrt+s.startIdx+idx]
+func (s *Sublist) Set(i int, value int) {
+	s.parent.Set(s.start+i, value)
 }
 
-func (s *Sublist) StartPtr() int {
-	return s.parent.StartPtr() + s.startIdx
+func (s *Sublist) Sublist(from, to int) GenericList {
+	return NewSublist(s.parent, s.start+from, s.start+to)
 }
 
-func (s *Sublist) Disk() *Disk {
-	return s.parent.Disk()
+func (s *Sublist) Length() int {
+	return s.end - s.start + 1
 }
 
-type Interpreter struct {
-	disk      *Disk
-	sequences map[string]Sequence
-	writer    *bufio.Writer
+func (s *Sublist) Add(value int) {
+	panic("Add is only available for root lists")
 }
 
-func NewInterpreter(disk *Disk, writer *bufio.Writer) *Interpreter {
-	return &Interpreter{
-		disk:      disk,
-		sequences: make(map[string]Sequence),
-		writer:    writer,
-	}
-}
+func executeCommand(line string, memory *Namespace, writer *bufio.Writer) {
+	line = strings.TrimSpace(line)
 
-func (i *Interpreter) AddSequence(name string, seq Sequence) {
-	i.sequences[name] = seq
-}
-
-func (i *Interpreter) GetSequence(name string) Sequence {
-	return i.sequences[name]
-}
-
-func (i *Interpreter) ExecuteCommand(line string) {
-	if strings.HasPrefix(line, "List") {
+	if strings.HasPrefix(line, "List ") && strings.Contains(line, "= new List(") {
 		parts := strings.Split(line, "=")
 		name := strings.TrimSpace(strings.Fields(parts[0])[1])
 		right := strings.TrimSpace(parts[1])
 
-		if strings.HasPrefix(right, "new List") {
-			content := right[len("new List(") : len(right)-1]
-			nums := strings.Split(content, ",")
+		valuesStr := right[len("new List(") : len(right)-1]
+		var values []int
 
-			elems := make([]int, len(nums))
-
-			for idx, s := range nums {
-				elems[idx], _ = strconv.Atoi(s)
+		if valuesStr != "" {
+			valueStrs := strings.Split(valuesStr, ",")
+			values = make([]int, len(valueStrs))
+			for i, str := range valueStrs {
+				val, _ := strconv.Atoi(strings.TrimSpace(str))
+				values[i] = val
 			}
-
-			lst := NewList(i.disk, name, elems...)
-			i.AddSequence(name, lst)
-		} else if strings.Contains(right, ".subList") {
-			left := strings.Split(right, ".subList")[0]
-
-			seq := i.GetSequence(left)
-
-			content := right[strings.Index(right, "(")+1 : len(right)-1]
-			bounds := strings.Split(content, ",")
-
-			from, _ := strconv.Atoi(bounds[0])
-			to, _ := strconv.Atoi(bounds[1])
-
-			sub := NewSublist(name, seq, from, to)
-			i.AddSequence(name, sub)
 		}
-	} else if strings.Contains(line, ".set") {
-		dot := strings.Index(line, ".")
-		name := line[:dot]
-		seq := i.GetSequence(name)
-		args := line[dot+len(".set(") : len(line)-1]
-		idxVal := strings.Split(args, ",")
-		idx, _ := strconv.Atoi(idxVal[0])
-		val, _ := strconv.Atoi(idxVal[1])
-		seq.Set(idx-1, val)
-	} else if strings.Contains(line, ".add") {
-		parts := strings.Split(line, ".")
-		name := parts[0]
 
-		seq := i.GetSequence(name)
+		memory.Set(name, NewList(values))
 
-		val, _ := strconv.Atoi(parts[1][len("add(") : len(parts[1])-1])
-		if l, ok := seq.(*List); ok {
-			l.Add(val)
-		}
-	} else if strings.Contains(line, ".get") {
-		parts := strings.Split(line, ".")
-		name := parts[0]
+	} else if strings.HasPrefix(line, "List ") && strings.Contains(line, ".subList(") {
+		parts := strings.Split(line, "=")
+		newName := strings.TrimSpace(strings.Fields(parts[0])[1])
+		right := strings.TrimSpace(parts[1])
 
-		seq := i.GetSequence(name)
+		sourceName := strings.Split(right, ".subList(")[0]
+		source := memory.Get(sourceName)
 
-		idx, _ := strconv.Atoi(parts[1][len("get(") : len(parts[1])-1])
-		i.writer.WriteString(strconv.Itoa(seq.Get(idx - 1)))
-		i.writer.WriteByte('\n')
+		argsStr := right[strings.Index(right, "(")+1 : len(right)-1]
+		args := strings.Split(argsStr, ",")
+		start, _ := strconv.Atoi(strings.TrimSpace(args[0]))
+		end, _ := strconv.Atoi(strings.TrimSpace(args[1]))
+
+		start0based := start - 1
+		end0based := end - 1
+
+		memory.Set(newName, source.Sublist(start0based, end0based))
+
+	} else if strings.Contains(line, ".get(") {
+		dotIndex := strings.Index(line, ".get(")
+		name := line[:dotIndex]
+		argsStr := line[dotIndex+len(".get(") : len(line)-1]
+
+		lst := memory.Get(name)
+		index, _ := strconv.Atoi(strings.TrimSpace(argsStr))
+		index0based := index - 1
+
+		writer.WriteString(strconv.Itoa(lst.Get(index0based)))
+		writer.WriteByte('\n')
+
+	} else if strings.Contains(line, ".set(") {
+		dotIndex := strings.Index(line, ".set(")
+		name := line[:dotIndex]
+		argsStr := line[dotIndex+len(".set(") : len(line)-1]
+
+		lst := memory.Get(name)
+		args := strings.Split(argsStr, ",")
+		index, _ := strconv.Atoi(strings.TrimSpace(args[0]))
+		value, _ := strconv.Atoi(strings.TrimSpace(args[1]))
+
+		index0based := index - 1
+		lst.Set(index0based, value)
+
+	} else if strings.Contains(line, ".add(") {
+		dotIndex := strings.Index(line, ".add(")
+		name := line[:dotIndex]
+		argsStr := line[dotIndex+len(".add(") : len(line)-1]
+
+		lst := memory.Get(name)
+		value, _ := strconv.Atoi(strings.TrimSpace(argsStr))
+		lst.Add(value)
 	}
 }
 
 func main() {
-	reader := bufio.NewReader(os.Stdin)
-	writer := bufio.NewWriter(os.Stdout)
+	reader := bufio.NewReaderSize(os.Stdin, 1<<23)
+	writer := bufio.NewWriterSize(os.Stdout, 1<<23)
 	defer writer.Flush()
 
 	nLine, _ := reader.ReadString('\n')
 	n, _ := strconv.Atoi(strings.TrimSpace(nLine))
 
-	commands := []string{}
+	memory := NewNamespace()
 
 	for range n {
 		line, _ := reader.ReadString('\n')
-		line = strings.TrimSpace(line)
-
-		commands = append(commands, line)
-	}
-
-	disk := NewDisk(2_000_000)
-
-	interp := NewInterpreter(disk, writer)
-
-	for _, cmd := range commands {
-		interp.ExecuteCommand(cmd)
+		executeCommand(line, memory, writer)
 	}
 }
